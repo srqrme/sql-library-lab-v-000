4115ae9c7d53d9e6755f785d208edae18ac456e0
Sharmaine Requerme <srqrme@gmail.com>
2018-11-29 17:54:59 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,14 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books WHERE series_id = 1 ORDER BY year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters WHERE LENGTH(motto) > 24 LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
